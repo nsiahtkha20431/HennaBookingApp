e94afadb3e9a4894ca9b1549e709c489df5a133a
Nishat <nishat20431@hotmail.ca>
2022-11-28 21:51:09 -0500

    Creating the booking form for users to create bookings

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My name is Nishat.
-       </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My name is Nishat. Welcome to my henna booking form!</p>
       </header>
     </div>
   );
