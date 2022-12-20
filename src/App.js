f61584193dbf4d066fa82590873e667440b132eb
Nishat Khan <nishatkhan20431@hotmail.ca>
2022-12-20 13:03:55 -0500

    Fixed css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const initialFormData = Object.freeze({
+    fname: "",
+    lname: ""
+  });
+
+  const [formData, updateFormData] = React.useState(initialFormData);
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+
+      // Trimming any whitespace
+      [e.target.name]: e.target.value.trim()
+    });
+  };
+    
+  const handleSubmit = (event) => {
+    // console.log("Nishat");
+    console.log(event.target.username.value);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>My name is Nishat. Welcome to my henna booking form!</p>
         <p>Please complete the following details to create your booking: </p>
         <form>
-          <label for="fname">First Name:</label>
-          <input type="text" id="fname" name="fname"></input><br></br>
-          <label for="lname">Last Name:</label>
-          <input type="text" id="lname" name="lname"></input><br></br>
-          <label for="phone">Phone Number</label>
-          <input type="text" id="phone" name="phone"></input><br></br>
-          <button type="button" onclick="alert('Hello world!')">Click Me!</button>
+          <button type="button">1 PM</button><br></br>
+          <button type="button">2 PM</button><br></br>
+          <button type="button">3 PM</button><br></br>
+          <button type="button">4 PM</button><br></br>
+          <button type="button">5 pm</button><br></br>
+          <button type="button">6 pm</button><br></br>
+          
+          <label htmlFor="fname">First Name:</label>
+          <input type="text" name="fname" onChange={handleChange}></input><br></br>
+          <label htmlFor="lname">Last Name:</label>
+          <input type="text" name="lname" onChange={handleChange}></input><br></br>
+          <label htmlFor="phone">Phone Number</label>
+          <input type="text" name="phone" onChange={handleChange}></input><br></br>
+          <button type="submit" onClick={handleSubmit}>Book!</button>
         </form>
       </header>
     </div>
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,33 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { cleanup, render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+afterEach(cleanup);
+
+// it('renders the component', () => {
+//     const {getByText} = render(<App/>);
+//     expect(getByText('Hello World')).toBeInTheDocument();
+// })
+
+it('submits the form', () => {
+    const onSubmit = jest.fn();
+    const { getByTestId, getByLabelText } = render(
+      <App onSubmit={onSubmit} />
+    );
+  
+    const nameInput = getByLabelText('Name:');
+    const emailInput = getByLabelText('Email:');
+    const form = getByTestId('my-form');
+  
+    fireEvent.change(nameInput, { target: { value: 'John' } });
+    fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+    fireEvent.submit(form);
+  
+    expect(onSubmit).toHaveBeenCalledWith({
+      name: 'John',
+      email: 'john@example.com',
+    });
+  });
