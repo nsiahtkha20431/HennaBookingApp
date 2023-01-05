d5702ba26b7b3cf88e3f71c78c1a9a293995282f
Nishat Khan <nishatkhan20431@hotmail.ca>
2023-01-05 12:42:31 -0500

    Added some comments to help me understand the code better

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = 3001; //port for the server to listen on 
 
-app.use(cors());
+app.use(cors()); //allows web pages from any domain to make requests on the server (not automatically allowed becasue of security issues)
 app.use(express.json());
 
-app.post('/', (req, res) => {
+app.post('/', (req, res) => { 
     console.log(req.body);
     res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Henna App listening on port ${PORT}`);
+app.listen(PORT, () => { //function to start a server that listens on the PORT
+  console.log(`Henna App listening on port ${PORT}`); //when server is ready, callback funtion prnts this
 });
 
