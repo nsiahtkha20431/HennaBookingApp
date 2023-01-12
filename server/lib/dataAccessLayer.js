da5e6aabf2f80a09bf1defe401390c634e52abd3
Nishat Khan <nishatkhan20431@hotmail.ca>
2023-01-12 18:08:12 -0500

    Successfully saving all subsequent booking info to database.json

--- server/lib/dataAccessLayer.js
+++ server/lib/dataAccessLayer.js
@@ -1,12 +1,15 @@
 import fs from 'fs';
 
-const FILE_PATH = './database.json';
+const FILE_PATH = '../database.json';
 
 const writeToFile = (newBookingEntry) => {
     console.log(newBookingEntry);
-    const fileData = fs.readFileSync(FILE_PATH);
-    console.log(fileData);
-    // fs.writeFileSync(FILE_PATH, JSON.stringify(newBookingEntry));
-  }
+    const fileData = fs.readFileSync(FILE_PATH, 'utf-8');
+
+    const parsedData = JSON.parse(fileData);
+    parsedData.push(newBookingEntry);
+
+    fs.writeFileSync(FILE_PATH, JSON.stringify(parsedData, null, 4), 'utf-8');
+};
 
   export default writeToFile;
